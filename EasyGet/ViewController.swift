45117e22dbb6779f3e9e90e4403ab17114e57a7d
Darkhonbek <darhonbek@gmail.com>
2019-04-21 19:07:03 +0500

    Initial Commit

---
+++ EasyGet/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  EasyGet
+//
+//  Created by Darkhonbek Mamataliev on 21/4/19.
+//  Copyright © 2019 Darkhonbek Mamataliev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
